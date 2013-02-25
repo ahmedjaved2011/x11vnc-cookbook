3c68aba61ef2a79254b0d16ce3dbf3a497421d1c
Ahmed Javed <ahmed.javed@gmail.com>
2013-02-24 21:31:54 -0500

    Added port attribute and fixed template for upstart
    Tested on xubuntu 12.10

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,25 +4,27 @@
 
 package "x11vnc"
 
-service "x11vnc" do
-  action [:enable, :start]
-  provider Chef::Provider::Service::Upstart
-end
-
 password_opt = ""
 unless node[:x11vnc][:password].empty?
   password_opt = "-rfbauth /etc/x11vnc.pass"
 
-  execute "gen x11vnc password file" do
+  execute "generate x11vnc password file" do
     command "x11vnc -storepasswd #{node[:x11vnc][:password]} /etc/x11vnc.pass"
   end
 end
 
 template "/etc/init/x11vnc.conf" do
   source "x11vnc.conf.erb"
-  mode 00644
+  mode 0644
   variables(
-    :password_opt => password_opt
+    :password_opt => password_opt,
+    :port => node[:x11vnc][:port]
   )
 end
+
+service "x11vnc" do        
+  action [:enable, :start]
+  provider Chef::Provider::Service::Upstart
+end
+
 
